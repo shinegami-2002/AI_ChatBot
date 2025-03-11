99366693a3ab5555e06cacd55c7d75563e8d37da
Chatadi Shanmukha <83128965+shinegami-2002@users.noreply.github.com>
2025-03-11 07:26:08 -0400

    added ai_chatbot files

---
+++ Agentic_chatbot/backend.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from pydantic import BaseModel
+from typing import List
+
+
+class RequestState(BaseModel):
+    model_name: str
+    model_provider: str
+    system_prompt: str
+    messages: List[str]
+    allow_search: bool
+
+from fastapi import FastAPI
+from ai_agent import get_response_from_ai_agent
+
+ALLOWED_MODEL_NAMES=["llama3-70b-8192", "mixtral-8x7b-32768", "llama-3.3-70b-versatile", "gpt-4o-mini"]
+
+app = FastAPI(title="AI agent") 
+
+@app.post("/chat")
+def chat_endpoint(request: RequestState):
+    """
+    just an endpoint for ai chatbot
+    """
+    if request.model_name not in ALLOWED_MODEL_NAMES:
+        return {"error": "Invalid model name. Kindly select a valid AI model"}
+    
+    llm_id = request.model_name
+    query = request.messages
+    allow_search = request.allow_search
+    system_prompt = request.system_prompt
+    provider = request.model_provider
+
+    response=get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider)
+    return response
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=9999) 
---
+++ Agentic_chatbot/frontend.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+st.set_page_config(page_title="LangGraph agent", layout="centered")
+st.title("AI Chatbot")
+st.write("This is a simple chatbot that uses the LangGraph AI agent to generate responses.")
+
+system_prompt = st.text_area("Define you agent:", height = 100, placeholder="Define your agent here..")
+
+MODEL_NAMES_GROQ = [ "mixtral-8x7b-32768", "llama-3.3-70b-versatile"]
+MODEL_NAMES_OPENAI = ["gpt-4o-mini"]    
+
+provider=st.radio("select the provider u want to use", ("Groq", "OpenAI"))
+if provider == "Groq":
+    selected_model = st.selectbox("Select Groq Model:", MODEL_NAMES_GROQ)
+elif provider == "OpenAI":
+    selected_model = st.selectbox("Select OpenAI Model:", MODEL_NAMES_OPENAI)
+
+allow_web_search=st.checkbox("Allow Web Search")
+
+system_prompt = st.text_area("Enter your query:", height = 100, placeholder="Ask away..")
+
+
+if st.button("Ask Agent!"):
+    if user_query.strip():
+        import requests
+
+        payload={
+            "model_name": selected_model,
+            "model_provider": provider,
+            "system_prompt": system_prompt,
+            "messages": [user_query],
+            "allow_search": allow_web_search
+        }
+
+        response=requests.post(API_URL, json=payload)
+        if response.status_code == 200:
+            response_data = response.json()
+            if "error" in response_data:
+                st.error(response_data["error"])
+            else:
+                st.subheader("Agent Response")
+                st.markdown(f"**Final Response:** {response_data}")
